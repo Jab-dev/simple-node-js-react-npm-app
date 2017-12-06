1a4761214b28ae93e0be7d265c855b66edf4bca6
Alvaro Cañada Bastias <alvaro.canada@bbva.com>
2017-12-06 16:16:43 +0100

    pruebas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to wonderland</h1>
+          <h1 className="App-title">Welcome to wonderland on develop branch</h1>
         </header>
         <p className="App-intro">
           To get started just whisper my name.
